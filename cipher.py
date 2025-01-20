697d2d69e7ccf8f9b92c469b073e9dca34ef18dd
OleksandrHub <sasha.klishch1@gmail.com>
2025-01-20 17:40:55 +0200

    Implement encryption logic for the simple cipher

--- cipher.py
+++ cipher.py
@@ -1,6 +1,12 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-print(index)
-shifted = alphabet[index]
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        print('space!')
+    index = alphabet.find(char)
+    new_index = index + shift
+    encrypted_text += alphabet[new_index]
+    print('char:', char, 'encrypted text:', encrypted_text)
